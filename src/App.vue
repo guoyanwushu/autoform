d79a6dc2becab3d07bf697dcc018722e8f430dbb
gang.yi <gang.yi@tellyes.com>
2020-07-22 17:11:48 +0800

    初步可以通过配置来实现页面呈现

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,73 @@
-<template>
-  <div id="app">
-    <el-row>
-      <el-col :span="4">
-        <div>
-          <el-button @click="addQueryPage">新增查询表单容器</el-button>
-          <el-button draggable="true" @dragstart.native="dragstart">文字输入框</el-button>
-        </div>
-      </el-col>
-      <el-col :span="16">
-        <draggable
-          class="syllable_ul"
-          element="div"
-          :options="{group:'row', animation:150}"
-          :no-transition-on-drag="true"
-        >
-          <component :is="pageContainer"></component>
-        </draggable>
-      </el-col>
-      <el-col :span="4"></el-col>
-    </el-row>
-  </div>
-</template>
 <script>
+import { containers, comps } from './util/map.js'
+import settings from './util/trans1'
 export default {
   data () {
     return {
-      pageContainer: ''
+      pageContainer: '',
+      name: '',
+      searchForm: {
+        class: '',
+        sex: '',
+        schoolNumber: ''
+      },
+      tableData: [{
+        sex: '女',
+        name: '小美',
+        schoolNumber: '123232'
+      }],
+      pages: {
+        total: 1,
+        currentPage: 1,
+        pageSize: 30
+      }
     }
   },
+  mounted () {
+  },
   methods: {
-    addQueryPage: function () {
-      this.pageContainer = require('./components/page_one.vue').default
-    },
-    dragstart (event) {
-      event.dataTransfer.setData('Text', 'page_one')
+    transToNode: function (setting, parentNode) {
+      // 生成vNdoe树， 然后把vNode树一编译，perfect
+      var self = this
+      if (setting.type === 'container') {
+        const node = containers[setting.name](self)
+        parentNode.children ? parentNode.children.push(node) : parentNode.children = [node]
+        if (setting.children.length) {
+          setting.children.map(function (item) {
+            self.transToNode(item, node)
+          })
+        }
+      } else {
+        // 分两种情况, 一种父组件是普通的标签元素，子元素是存在children里面。一种父组件是组件，子元素是存在componentOptions.children
+        // 里面
+        const node = comps[setting.comp_type](setting, self)
+        if (!parentNode.componentOptions) {
+          // 父节点是元素节点
+          parentNode.children = parentNode.children || []
+          parentNode.children.push(node)
+        } else {
+          parentNode.componentOptions.children = parentNode.componentOptions.children || []
+          parentNode.componentOptions.children.push(node)
+        }
+        if (setting.children && setting.children.length) {
+          setting.children.map(function (item) {
+            self.transToNode(item, node)
+          })
+        }
+      }
     }
+  },
+  render (c) {
+    var self = this
+    var root = c('div', {
+      domProps: {
+        id: 'app'
+      }
+    })
+    settings.map(function (item) {
+      self.transToNode(item, root)
+    })
+    return root
   }
 }
 </script>
@@ -48,11 +81,43 @@ export default {
     width: 100%;
     height: 100%;
     position: relative;
+    display: flex;
+    flex-direction: column;
   }
   .el-row {
     height: 100%;
   }
   .el-col {
     height: 100%;
+  }
+  .layout-search {
+    padding: 8px 8px 0;
+    border-bottom:1px solid #EBEEF5;
+    .el-form-item__content .el-select {
+      width: 200px;
+    }
+    .el-form-item--small.el-form-item {
+      margin-bottom: 8px !important;
+    }
+  }
+  .table-head {
+    border-top: 1px solid #e8e8e8;
+    border-bottom: 1px solid #e8e8e8;
+    color: rgba(0, 0, 0, 0.85);
+    font-weight: 700;
+    font-size: 14px;
+  }
+  .el-table--small th{
+    padding: 8px 0!important;
+  }
+  .el-table--small td{
+    padding: 5px 0!important;
+  }
+  /*
+    element table组件中el-table元素上cell-class-name属性对应的值，手动挂载，会在对应的元素上添加该类
+  */
+  .table-cell {
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.65);
   }
 </style>
--- src/util/trans1.js
+++ src/util/trans1.js
@@ -3,34 +3,41 @@ var layOut = [{
   name: 'search',
   children: [{
     type: 'component',
-    name: '_form_',
+    comp_type: '_form_',
     model: 'searchForm',
+    ref: 'searchForm',
     children: [{
       type: 'component',
       comp_type: 'formItem_input_text',
+      slot: true,
       label: '学号',
       valueField: 'searchForm.schoolNumber'
     }, {
       type: 'component',
       comp_type: 'formItem_input_text',
       label: '性别',
+      slot: true,
       valueField: 'searchForm.sex'
     }, {
       type: 'component',
       comp_type: 'formItem_input_text',
       label: '班级',
+      slot: true,
       valueField: 'searchForm.class'
     }, {
       type: 'component',
       comp_type: 'button_search',
       handlerType: 'function_table_search',
       handlerName: 'search',
+      slot: true,
       text: '搜索'
     }, {
       type: 'component',
       comp_type: 'button_reset',
       handlerType: 'function_table_search_reset',
       handlerName: 'resetForm',
+      slot: true,
+      formRef: 'searchForm',
       text: '重置'
     }]
   }]
@@ -45,7 +52,7 @@ var layOut = [{
     text: '新增'
   }, {
     type: 'component',
-    component: 'button_remove',
+    comp_type: 'button_remove',
     handlerType: 'function_batch_remove',
     handlerName: '_batch_remove',
     text: '批量移除'
@@ -55,15 +62,17 @@ var layOut = [{
   name: 'table',
   children: [{
     type: 'component',
-    component: 'table',
-    tableFields: [{
-      type: 'index'
-    }, {
+    comp_type: 'table',
+    tableData: 'tableData',
+    fields: [{
       label: '姓名',
-      field: 'name'
+      prop: 'name'
+    }, {
+      label: '性别',
+      prop: 'sex'
     }, {
       label: '学号',
-      field: 'schoolNumber'
+      prop: 'schoolNumber'
     }]
   }]
 }, {
@@ -71,7 +80,10 @@ var layOut = [{
   name: 'pagination',
   children: [{
     type: 'component',
-    comp_type: 'pagination'
+    comp_type: 'pagination',
+    total: 'pages.total',
+    pageSize: 'pages.pageSize',
+    currentPage: 'pages.currentPage'
   }]
 }]
 export default layOut
